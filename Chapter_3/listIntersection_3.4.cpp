d4561d4d3d7fc12eecd0517a08fcd80bafd05526
Braam Beresford <braam@braam.email>
2019-03-30 13:39:07 -0500

    Fixed naming and confusion around union and intersection algos

--- Chapter_3/listIntersection_3.4.cpp
+++ Chapter_3/listIntersection_3.4.cpp
@@ -9,14 +9,28 @@ std::list<int> listIntersection(const std::list<int> & l1, const std::list<int> 
 	std::list<int> result;
 	std::list<int>::const_iterator it1 = l1.begin();
 	std::list<int>::const_iterator it2 = l2.begin();
-
+	while (it1 != l1.end() && it2 != l2.end()) {
+		if (*it1 == *it2) {
+			result.push_back(*it1++);
+			it2++;
+		}
+		else if (*it1 < *it2) {
+			it1++;
+		}
+		else if (*it2 < *it1) {
+			it2++;
+		}
+		else {
+			throw std::invalid_argument( "Option in decision tree that should not be triggered has been triggered" );
+		}
+	}
 	return result;
 }
 
 
 
 int main() {
-	std::list<int> l1 {1, 3, 5, 6, 7};
+	std::list<int> l1 {1, 3, 5, 6, 7, 9};
 	std::list<int> l2 {2, 3, 4, 6, 8, 9};
 	auto result = listIntersection(l1, l2);
 	std::cout << '\n' << std::endl;
