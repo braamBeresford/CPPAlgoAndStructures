fab47b82b7678bf868c080e0ef9c61c47b386fc9
Braam Beresford <braam@braam.email>
2019-03-30 13:09:13 -0500

    Complete union of two lists using only STL list methods

--- listUnion_3.5.cpp
+++ listUnion_3.5.cpp
@@ -13,42 +13,14 @@ std::list<int> listIntersection(const std::list<int> & l1, const std::list<int> 
 	std::list<int>::const_iterator it2 = l2.begin();
 
 	while (it1 != l1.end()) {
-		// std::cout << "It1: " << *it1 << " it2: " << *it2 << std::endl;
-		for (auto elem : result) {
-			std::cout << elem << std::endl;
-		}
-		if (it2 != l2.end() && *it1 < *it2) {
+		if (it2 == l2.end() || (it2 != l2.end() && *it1 < *it2)) {
 			result.push_back(*it1++);
 		}
 		else if (it2 != l2.end() && *it1 == *it2) {
 			result.push_back(*it1++);
 			it2++;
 		}
-		else if (it2 == l2.end()) {
-			while (it1 != l1.end()) {
-				result.push_back(*it1++);
-			}
-			break;
-		}
-		else {
-
-			while (it2 != l2.end()) {
-
-				if (it1 == l1.end()) {
-					result.push_back(*it2++);
-				}
-				else if (*it2 < *it1) {
-					result.push_back(*it2++);
-				}
-				else {
-					break;
-				}
-			}
-		}
-	}
-
-	if (it2 != l2.end()) {
-		while (it2 != l2.end()) {
+		while ((it2 != l2.end() && it1 == l1.end()) || (it2 != l2.end() && *it2 < *it1)) {
 			result.push_back(*it2++);
 		}
 	}
@@ -58,8 +30,8 @@ std::list<int> listIntersection(const std::list<int> & l1, const std::list<int> 
 
 
 int main() {
-	std::list<int> l1 {1, 3, 5, 6, 7, 9};
-	std::list<int> l2 {2, 3, 4, 6, 8};
+	std::list<int> l1 {1, 3, 5, 6, 7};
+	std::list<int> l2 {2, 3, 4, 6, 8, 9};
 	auto result = listIntersection(l1, l2);
 	std::cout << '\n' << std::endl;
 	for (auto elem : result) {
